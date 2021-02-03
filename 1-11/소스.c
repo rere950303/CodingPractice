6f041555827814b8877b1d8ee8082b6cd91a8296
HyungCom\양형욱 <yhwjjang1995@naver.com>
2021-02-03 23:23:46 +0900

    feat: AlsongDalsong solve 1-11 ~ 2-7

---
+++ 1-11/소스.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int dup;
+	
+	
+	
+	printf("정수1 입력:\n");
+	scanf_s("%d", &a);
+
+	printf("정수2 입력:\n");
+	scanf_s("%d", &b);
+
+	dup = a * b;
+
+	printf("곱셈결과:%d\n", dup);
+
+	return 0;
+}
---
+++ 2-3/소스.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	printf("정수1을 입력하세요:");
+	scanf_s("%d", &a);
+
+	printf("정수2을 입력하세요:");
+	scanf_s("%d", &b);
+
+	if (a > b)
+		printf("정수2개의 비율:%d%%\n", b*100/a);
+
+	if (a < b)
+		printf("정수2개의 비율:%d%%\n", a*100/b);
+
+    if (a==b)
+		printf("정수2개의 비율:%d%%\n", 100);
+
+	return 0;
+}
